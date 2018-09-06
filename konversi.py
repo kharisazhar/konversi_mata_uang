ff58c4f88e2d9963a8777791057841933a7c4dfb
kharisazhar <muhammadkharisazhar149a@gmail.com>
2018-09-06 15:18:10 +0700

    first

---
+++ konversi.py
@@ -0,0 +1,38 @@
+import rupiah
+
+vinput = "Y"
+
+
+def usdtoidr(usd):
+    a = usd * 15000
+    return rupiah.formatrupiah(a)
+
+
+def idrtousd(idr):
+    a = idr / float(15000)
+    return a
+
+
+print ("PROGRAM KONVERSI MATA UANG")
+print ("==========================")
+print ("MENU PILIHAN :")
+print ("1. KONVERSI DOLLAR KE RUPIAH")
+print ("2. KONVERSI RUPIAH KE DOLLAR")
+print ("3. KELUAR")
+print ("==========================")
+
+while (raw_input("ketik [Y/N] untuk lanjut :") == vinput):
+    jawab = int(raw_input("Pilih Menu Konversi :"))
+    if (jawab == 1):
+        print ("Konversi dollar ke rupiah")
+        jml = int(raw_input("Masukan Jumlah Dollar : "))
+        print jml, " dollar = ", usdtoidr(jml)
+    elif (jawab == 2):
+        print ("Konversi rupiah ke dollar")
+        jml = int(raw_input("Masukan Jumlah Rupiah :"))
+        print rupiah.formatrupiah(jml), " = $", idrtousd(jml)
+    elif (jawab == 3):
+        vinput == "N"
+    else:
+        vinput == "N"
+print "Terimakasih"
